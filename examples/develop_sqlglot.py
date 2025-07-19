6c515e3d62c9af38f706533028e5d8ab859fd481
Erich Silva <erich.hs13@gmail.com>
2025-07-18 17:49:25 -0700

    Added support for more data types

--- examples/develop_sqlglot.py
+++ examples/develop_sqlglot.py
@@ -1,6 +1,8 @@
 import yaml
 from typing import Dict, Callable, Any
 from sqlglot import exp, parse_one
+from sqlglot.expressions import convert
+
 
 # %% Loading YADS spec
 yaml_file = "examples/specs/yads_spec.yaml"
@@ -13,7 +15,7 @@ _YADS_TO_SQLGLOT_TYPE_MAP: Dict[str, exp.DataType.Type] = {
     "integer": exp.DataType.Type.INT,
     "date": exp.DataType.Type.DATE,
     "decimal": exp.DataType.Type.DECIMAL,
-    "string": exp.DataType.Type.VARCHAR,
+    "string": exp.DataType.Type.TEXT,
     "timestamp_tz": exp.DataType.Type.TIMESTAMPTZ,
     "array": exp.DataType.Type.ARRAY,
     "struct": exp.DataType.Type.STRUCT,
@@ -26,6 +28,12 @@ _YADS_TO_SQLGLOT_TRANSFORM_MAP: Dict[str, type[exp.Func]] = {
     "day": exp.Day,
 }
 
+_YADS_TO_SQLGLOT_CONSTRAINT_MAP: Dict[str, type[exp.ColumnConstraintKind]] = {
+    "not_null": exp.NotNullColumnConstraint,
+    "primary_key": exp.PrimaryKeyColumnConstraint,
+    "default": exp.DefaultColumnConstraint,
+}
+
 # Namespace
 namespace = spec["name"].split(".")
 table_name = namespace[-1]
@@ -33,48 +41,39 @@ database_name = namespace[-2]
 catalog_name = namespace[-3]
 
 # Options
-options = spec["options"]
+options = spec.get("options", {})
 
 # Properties
-properties = spec["properties"]
-print(properties)
+properties = spec.get("properties", {})
 
 # Metadata
-metadata = spec["metadata"]
+metadata = spec.get("metadata", {})
 
 # Columns
-columns = spec["columns"]
+columns = spec.get("columns", [])
+
 
 # %% Base SQL DDL
 sql_ddl = """
 CREATE TABLE warehouse.orders.customer_orders (
-    order_id UUID NOT NULL
-)
-PARTITIONED BY (order_date, MONTH(created_at))
-LOCATION '/warehouse/orders/customer_orders'
-TBLPROPERTIES (
-    'table_type'='iceberg',
-    'format'='parquet',
-    'write_compression'='snappy'
+    tags ARRAY<STRING>
 )
 """
+# PARTITIONED BY (order_date, MONTH(created_at))
+# LOCATION '/warehouse/orders/customer_orders'
+# TBLPROPERTIES (
+#     'table_type'='iceberg',
+#     'format'='parquet',
+#     'write_compression'='snappy'
+# )
 
 ast_from_sql = parse_one(sql_ddl)
 
+
 # %% AST from spec
 
-"""
-properties = {
-    "partitioned_by": [ -> PartitionedByProperty
-        {"column": "order_date"},
-        {"column": "created_at", "transform": "month"}
-    ],
-    "location": "/warehouse/orders/customer_orders", -> LocationProperty
-    "table_type": "iceberg", -> Property
-    "format": "parquet", -> Property
-    "write_compression": "snappy" -> Property
-}
-"""
+## Property handlers
+PropertyHandler = Callable[[Any], exp.Property]
 
 
 def _create_partition_expression(col_spec: dict) -> exp.Expression:
@@ -110,9 +109,6 @@ def _handle_generic_property(key: str, value: Any) -> exp.Property:
     )
 
 
-PropertyHandler = Callable[[Any], exp.Property]
-
-
 def _parse_properties(properties: dict) -> list[exp.Property]:
     property_handlers: dict[str, PropertyHandler] = {
         "partitioned_by": _handle_partitioned_by_property,
@@ -129,6 +125,62 @@ def _parse_properties(properties: dict) -> list[exp.Property]:
     return properties_expressions
 
 
+## Column definition handlers
+def _handle_array_column_data_type(col_spec: dict) -> exp.DataType:
+    assert "element" in col_spec, "Array column must have an element"
+    element_type = _handle_column_data_type(col_spec["element"])
+    return exp.DataType(
+        this=exp.DataType.Type.ARRAY,
+        expressions=[element_type],
+        nested=exp.DataType.Type.ARRAY in exp.DataType.NESTED_TYPES,
+    )
+
+
+def _handle_column_data_type(col_spec: dict) -> exp.DataType:
+    if _YADS_TO_SQLGLOT_TYPE_MAP.get(col_spec["type"]) == exp.DataType.Type.ARRAY:
+        return _handle_array_column_data_type(col_spec)
+
+    data_type_expressions = []
+    if "params" in col_spec:
+        for _, value in col_spec["params"].items():
+            data_type_expressions.append(
+                exp.DataTypeParam(this=convert(value)),
+            )
+    return exp.DataType(
+        this=_YADS_TO_SQLGLOT_TYPE_MAP[col_spec["type"]],
+        expressions=data_type_expressions if data_type_expressions else None,
+        nested=_YADS_TO_SQLGLOT_TYPE_MAP[col_spec["type"]] in exp.DataType.NESTED_TYPES,
+    )
+
+
+def _parse_column_def(col_spec: dict) -> exp.ColumnDef:
+    constraints = col_spec.get("constraints")
+    return exp.ColumnDef(
+        this=exp.Identifier(this=col_spec["name"]),
+        kind=_handle_column_data_type(col_spec),
+        constraints=_parse_column_constraints(constraints) if constraints else None,
+    )
+
+
+## Column constraint handlers
+def _handle_default_column_constraint(value: Any) -> exp.ColumnConstraint:
+    return exp.ColumnConstraint(kind=exp.DefaultColumnConstraint(this=convert(value)))
+
+
+def _parse_column_constraints(constraints: dict) -> list[exp.ColumnConstraint]:
+    column_constraints: list[exp.ColumnConstraint] = []
+    for constraint in constraints:
+        constraint_class = _YADS_TO_SQLGLOT_CONSTRAINT_MAP[constraint]
+        if constraint_class == exp.DefaultColumnConstraint:
+            column_constraints.append(
+                _handle_default_column_constraint(constraints[constraint])
+            )
+        else:
+            column_constraints.append(exp.ColumnConstraint(kind=constraint_class()))
+    return column_constraints
+
+
+## AST from spec
 ast_from_spec = exp.Create(
     this=exp.Schema(
         this=exp.Table(
@@ -136,24 +188,16 @@ ast_from_spec = exp.Create(
             db=exp.Identifier(this=database_name),
             catalog=exp.Identifier(this=catalog_name),
         ),
-        expressions=[
-            # for loop over columns here
-            exp.ColumnDef(
-                this=exp.Identifier(this=columns[0]["name"]),
-                kind=exp.DataType(
-                    this=_YADS_TO_SQLGLOT_TYPE_MAP[columns[0]["type"]],
-                    nested=_YADS_TO_SQLGLOT_TYPE_MAP[columns[0]["type"]]
-                    in exp.DataType.NESTED_TYPES,
-                ),
-                # for loop over column constraints here
-                constraints=[exp.ColumnConstraint(kind=exp.NotNullColumnConstraint())],
-            )
-        ],
+        expressions=[_parse_column_def(col) for col in columns],
     ),
     kind="TABLE",
     # exists=options.get("if_not_exists", False),
     # replace=options.get("or_replace", False),
-    properties=exp.Properties(expressions=_parse_properties(properties)),
+    properties=(
+        exp.Properties(expressions=_parse_properties(properties))
+        if properties
+        else None
+    ),
 )
 
 # %% Validate
@@ -166,5 +210,10 @@ print(repr(ast_from_spec))
 print("\n" + "=" * 60)
 
 print(f"AST are equal: {ast_from_sql == ast_from_spec}")
-print("SQL from AST:")
-print(ast_from_spec.sql(dialect="athena", pretty=True))
+print("\n" + "=" * 60)
+
+print("SQL from AST:\n")
+for dialect in ["athena", "spark"]:
+    print(f"Dialect: {dialect}\n")
+    print(ast_from_spec.sql(dialect=dialect, pretty=True))
+    print("\n" + "-" * 60)
