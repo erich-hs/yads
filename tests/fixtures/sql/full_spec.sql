b68d5af2588d13dd008bdd0a1d4e3ff0793ec0f3
Erich Silva <erich.hs13@gmail.com>
2025-07-29 19:02:22 -0700

    Increased scope of full_spec fixtures

--- tests/fixtures/sql/full_spec.sql
+++ tests/fixtures/sql/full_spec.sql
@@ -1,11 +1,30 @@
 CREATE EXTERNAL TABLE catalog.db.full_schema (
-  user_id UUID NOT NULL,
-  score DECIMAL(5, 2),
-  CONSTRAINT pk_full_schema PRIMARY KEY (user_id)
+  c_uuid UUID NOT NULL,
+  c_string TEXT DEFAULT 'default_string',
+  c_string_len TEXT(255),
+  c_int8 TINYINT,
+  c_int16 SMALLINT,
+  c_int32_identity INT GENERATED ALWAYS AS IDENTITY(1, 1),
+  c_int64 BIGINT,
+  c_float32 FLOAT,
+  c_float64 DOUBLE,
+  c_boolean BOOLEAN,
+  c_decimal DECIMAL,
+  c_decimal_ps DECIMAL(10, 2),
+  c_date DATE NOT NULL,
+  c_timestamp TIMESTAMP,
+  c_binary BINARY,
+  c_interval_ym INTERVAL YEAR TO MONTH,
+  c_interval_d INTERVAL DAY,
+  c_array ARRAY<INT>,
+  c_map MAP<TEXT, DOUBLE>,
+  c_struct STRUCT<nested_int: INT, nested_string: TEXT>,
+  CONSTRAINT pk_full_schema PRIMARY KEY (c_uuid, c_date),
+  CONSTRAINT fk_other_table FOREIGN KEY (c_int64) REFERENCES other_table (id)
 )
 LOCATION '/data/full.schema'
 USING parquet
 TBLPROPERTIES (
   'write_compression' = 'snappy'
 )
-PARTITIONED BY (user_id, IDENTITY(score))
+PARTITIONED BY (c_string_len, month(c_date))
