f2182bb989cd60eb1fff95605257ac37e0d5cd6a
Erich Silva <erich.hs13@gmail.com>
2025-08-24 17:02:12 -0700

    Adding all validation rules to `conveters.sql` entrypoint

--- src/yads/converters/sql/__init__.py
+++ src/yads/converters/sql/__init__.py
@@ -1,7 +1,17 @@
 from .sql_converter import SQLConverter, SparkSQLConverter, DuckdbSQLConverter
 from .ast_converter import SQLGlotConverter
 from .validators.ast_validator import AstValidator
-from .validators.ast_validation_rules import AstValidationRule, DisallowFixedLengthString
+from .validators.ast_validation_rules import (
+    AstValidationRule,
+    DisallowType,
+    DisallowUserDefinedType,
+    DisallowFixedLengthString,
+    DisallowFixedLengthBinary,
+    DisallowNegativeScaleDecimal,
+    DisallowParameterizedGeometry,
+    DisallowColumnConstraintGeneratedIdentity,
+    DisallowTableConstraintPrimaryKeyNullsFirst,
+)
 
 __all__ = [
     "SQLConverter",
@@ -10,5 +20,12 @@ __all__ = [
     "SQLGlotConverter",
     "AstValidator",
     "AstValidationRule",
+    "DisallowType",
+    "DisallowUserDefinedType",
     "DisallowFixedLengthString",
+    "DisallowFixedLengthBinary",
+    "DisallowNegativeScaleDecimal",
+    "DisallowParameterizedGeometry",
+    "DisallowColumnConstraintGeneratedIdentity",
+    "DisallowTableConstraintPrimaryKeyNullsFirst",
 ]
