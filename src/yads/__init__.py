dca0af29bea82535cb31d0441b83aba843e5aa6d
Erich Silva <erich.hs13@gmail.com>
2025-11-07 16:57:46 -0800

    Added `to_polars` converter

--- src/yads/__init__.py
+++ src/yads/__init__.py
@@ -1,12 +1,13 @@
 from .spec import YadsSpec
 from .loaders import from_yaml, from_dict, from_pyarrow
-from .converters import to_pyarrow, to_pydantic, to_pyspark, to_sql
+from .converters import to_polars, to_pyarrow, to_pydantic, to_pyspark, to_sql
 
 __all__ = [
     "YadsSpec",
     "from_yaml",
     "from_dict",
     "from_pyarrow",
+    "to_polars",
     "to_pyarrow",
     "to_pydantic",
     "to_pyspark",
